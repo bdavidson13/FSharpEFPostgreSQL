5ee23a4428aed6d0f9a93f7d79ed2b7cf62d3cb8
Brian Davidson <brian.j.davidson13@gmail.com>
2019-02-07 17:18:42 -0500

    initial commit of code

---
+++ FSharpEFPostgreSQL/CompositionRoot.fs
@@ -0,0 +1,17 @@
+﻿module CompostionRoot
+
+open EFCore.DataAccess
+open Microsoft.EntityFrameworkCore
+let configurePostgresServerContext =
+    (fun() ->
+        let optionBuilder = new DbContextOptionsBuilder<CharityContext>();
+        optionBuilder.UseNpgsql("User ID=root;Password=*******;Server=localhost;Port=5432;Database=charity;Pooling=true;") |>ignore
+        new CharityContext(optionBuilder.Options)
+    )
+
+let getContext = configurePostgresServerContext()
+let getUserAccount  = UserAccountsRepository.getUserAccount getContext
+let addUserAccount = UserAccountsRepository.addUserAccount getContext
+let addUserAccountAsync = UserAccountsRepository.addUserAccountAsync getContext
+let updateUserAccount = UserAccountsRepository.updateUserAccount getContext
+let deleteUserAccount = UserAccountsRepository.deleteUserAccount getContext
---
+++ FSharpEFPostgreSQL/UserAccountsRepository.fs
@@ -0,0 +1,33 @@
+﻿namespace EFCore.DataAccess
+open EFCore.DataAccess
+open EFCore.Model
+open System.Linq
+open Microsoft.EntityFrameworkCore
+module UserAccountsRepository =
+    let getUserAccount (context: CharityContext) id =
+        query {
+            for userAccount in context.UserAccount do
+                where (userAccount.id = id)
+                select userAccount 
+                exactlyOne
+        } |> (fun x -> if box x = null then None else Some x)
+
+    let addUserAccountAsync (context: CharityContext) (entity: UserAccount) =
+        async {
+            context.UserAccount.AddAsync(entity) |> Async.AwaitTask |> ignore
+            let! _ = context.SaveChangesAsync true |> Async.AwaitTask
+            return entity
+        }    
+
+    let addUserAccount (context: CharityContext) (entity: UserAccount) =
+        context.UserAccount.Add(entity) |> ignore
+        context.SaveChanges true |> ignore
+
+    let updateUserAccount (context: CharityContext) (entity: UserAccount) = 
+        let currentEntry = context.UserAccount.Find(entity.id)
+        context.Entry(currentEntry).CurrentValues.SetValues(entity)
+        context.SaveChanges true |> ignore
+
+    let deleteUserAccount (context: CharityContext) (entity: UserAccount) = 
+        context.UserAccount.Remove entity |> ignore
+        context.SaveChanges true |> ignore
